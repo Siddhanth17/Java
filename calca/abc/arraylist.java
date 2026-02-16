c9c6fecd9eeaa51f0d27599aacdc7269dfae3925
Siddhanth <siddhanthdalal@gmail.com>
2026-02-16 10:41:22 +0530

    Array list

---
+++ calca/abc/arraylist.java
@@ -0,0 +1,18 @@
+public class arraylist {
+    public static void main(String[] args) {
+        java.util.ArrayList<Integer> list = new java.util.ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+        for (int num: list) {
+            System.out.println(num);
+        }
+    }
+}
