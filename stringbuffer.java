22ad029b80a005a2eb2c50962019d29582bd93f4
Siddhanth <siddhanthdalal@gmail.com>
2026-02-17 13:02:18 +0530

    String buffer exmaple

---
+++ stringbuffer.java
@@ -0,0 +1,19 @@
+public class stringbuffer{
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Hello");
+        sb.append(" World");
+        System.out.println(sb.toString());
+
+        sb.insert(5, ",");
+        System.out.println(sb.toString());
+
+        sb.replace(0, 5, "Hi");
+        System.out.println(sb.toString());
+
+        sb.delete(2, 7);
+        System.out.println(sb.toString());
+
+        sb.reverse();
+        System.out.println(sb.toString());
+    }
+}
